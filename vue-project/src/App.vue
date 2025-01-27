945dc409e820f1394d9b29bdcf47584221129c06
Sondrick Frondozo <119903912+SnFrondozo@users.noreply.github.com>
2025-01-27 14:46:16 +0800

    updated

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,13 +1,9 @@
 <template>
   <h1>Food</h1>
-  <personal-profile/> <br>
-  <food-item/> <br>
-  <food-item2/> <br>
-  <food-item/> <br>
-  
+  <food-item />
+  <food-item2 />
 </template>
 
 <script></script>
 
-<style>
-</style>           
+<style></style>
