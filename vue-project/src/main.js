604613f5cd4ab8c10ef07c87c471b01a69c28228
Sondrick Frondozo <119903912+SnFrondozo@users.noreply.github.com>
2025-02-10 01:40:10 +0000

    updated

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -4,10 +4,16 @@ import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
 import PersonalProfile from './components/PersonalProfile.vue'
+import CommentForm from './components/CommentForm.vue'
+import Country from './components/Country.vue'
+import Comment from './components/Comment.vue'
 
 const app = createApp(App)
 
 app.component('food-item', FoodItem)
+app.component('country', Country)
+app.component('comment-form', CommentForm)
+app.component('comment', Comment)
 app.component('food-item2', FoodItem2)
 app.component('personal-profile', PersonalProfile)
 
